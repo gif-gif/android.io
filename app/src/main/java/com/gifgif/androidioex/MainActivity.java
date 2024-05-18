5d05da87a05ac3153144f48f49f44a3fba691e7d
姚吉日 <gif@live.cn>
2024-05-18 22:43:58 +0800

    bugfix

--- app/src/main/java/com/gifgif/androidioex/MainActivity.java
+++ app/src/main/java/com/gifgif/androidioex/MainActivity.java
@@ -1,7 +1,8 @@
-package com.gifgif.androidio;
+package com.gifgif.androidioex;
 
 import android.os.Bundle;
 
+import com.gifgif.androidioex.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,6 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.gifgif.androidio.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,12 +26,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
